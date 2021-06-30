a03f701c0474e90537f1b06a9c307b533e51baa2
Tomer Babila <tomerb@academy-skills.com>
2021-06-30 16:25:22 +0300

    Reset deleted flags

--- client/src/app/components/flags/flags.component.ts
+++ client/src/app/components/flags/flags.component.ts
@@ -8,19 +8,28 @@ import { FlagService } from 'src/app/services/flag.service';
   styleUrls: ['./flags.component.scss'],
 })
 export class FlagsComponent implements OnInit {
+  allFlags: any;
   flags: any;
 
   constructor(private flagService: FlagService) {}
 
   ngOnInit(): void {
     this.getFlags();
+    this.allFlags = this.flags;
   }
 
   getFlags(): void {
-    this.flagService.getFlags().subscribe((data) => (this.flags = data));
+    this.flagService.getFlags().subscribe((data) => {
+      this.flags = data;
+      this.allFlags = data;
+    });
   }
 
   deleteFlag(name: string): void {
     this.flags = this.flags.filter((f: any) => f.name !== name);
+  }
+
+  resetFlags(): void {
+    this.flags = this.allFlags;
   }
 }
