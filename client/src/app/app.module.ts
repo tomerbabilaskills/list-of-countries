1409f81243da175c481adae9bb3e89d5b49d17f3
Tomer Babila <tomerb@academy-skills.com>
2021-07-01 12:11:52 +0300

    Add responsive elements to the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FlagsComponent } from './components/flags/flags.component';
 import { SearchComponent } from './components/search/search.component';
 import { FlagComponent } from './components/flag/flag.component';
+import { ResetButtonComponent } from './components/reset-button/reset-button.component';
 
 @NgModule({
-  declarations: [AppComponent, FlagsComponent, SearchComponent, FlagComponent],
+  declarations: [AppComponent, FlagsComponent, SearchComponent, FlagComponent, ResetButtonComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
