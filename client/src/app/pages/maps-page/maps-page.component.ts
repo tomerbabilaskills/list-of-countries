ef36ad4bf4f00953bc454e58057660f91c70ac8e
Tomer Babila <tomerb@academy-skills.com>
2021-07-05 12:46:17 +0300

    Add autocomplete
    Re-center and create a new marker at the chosen location

--- client/src/app/pages/maps-page/maps-page.component.ts
+++ client/src/app/pages/maps-page/maps-page.component.ts
@@ -9,25 +9,57 @@ import {} from 'google.maps';
 })
 export class MapsPageComponent implements OnInit {
   @ViewChild('map', { static: true }) mapElement: any;
+  @ViewChild('autocomplete', { static: true }) autocompleteElement: any;
+
   map!: google.maps.Map;
+  autocomplete!: google.maps.places.Autocomplete;
+
   constructor() {}
 
   ngOnInit(): void {
+    // Moveo location
     const moveoPosition = { lat: 32.06472745555116, lng: 34.771794568855235 };
+
     const mapProperties = {
       center: new google.maps.LatLng(moveoPosition),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
 
+    // Center the map at Moveo location
     this.map = new google.maps.Map(
       this.mapElement.nativeElement,
       mapProperties
     );
 
-    const marker = new google.maps.Marker({
+    // Add marker to Moveo location
+    new google.maps.Marker({
       position: moveoPosition,
       map: this.map,
+    });
+
+    // Add autocomplete section
+    this.autocomplete = new google.maps.places.Autocomplete(
+      this.autocompleteElement.nativeElement,
+      {
+        types: ['address'],
+        fields: ['place_id', 'geometry', 'name'],
+      }
+    );
+
+    // Re-center and create a new marker at the chosen location
+    this.autocomplete.addListener('place_changed', () => {
+      const place = this.autocomplete.getPlace();
+      if (!place.geometry) {
+        console.log('Place not found');
+      } else {
+        new google.maps.Marker({
+          position: place.geometry.location,
+          map: this.map,
+        });
+        this.map.setCenter(place.geometry.location!);
+      }
+      this.autocompleteElement.nativeElement.value = '';
     });
   }
 }
